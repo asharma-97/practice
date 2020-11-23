99191996503e8ef8421d89167603a5d41654e98f
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-23 07:14:06 +0530

    1561 Max coins

--- src/main/scala/akka/actors/BasicExercise.scala
+++ src/main/scala/akka/actors/BasicExercise.scala
@@ -1,4 +1,4 @@
-package actors
+package akka.actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
---
+++ src/main/scala/codes/MaxCoins.java
@@ -0,0 +1,18 @@
+package codes;
+
+import java.util.Arrays;
+
+public class MaxCoins {
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxCoins(int[] piles) {
+        Arrays.sort(piles);
+        int sum = 0;
+        for (int i = piles.length - 2; i >= piles.length / 3; i -= 2) {
+            sum += piles[i];
+        }
+        return sum;
+    }
+}
