0863dc9a088d89776adaf0e78c423f895a845cc0
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-24 07:24:58 +0530

    1323 Maximum 69 num

---
+++ src/main/scala/codes/Maximum69Num.java
@@ -0,0 +1,12 @@
+package codes;
+
+public class Maximum69Num {
+    public static void main(String[] args) {
+
+    }
+    public int maximum69Number (int num) {
+        String s = Integer.toString(num);
+        String s1 = s.replaceFirst("6", "9");
+        return new Integer(s1);
+    }
+}
