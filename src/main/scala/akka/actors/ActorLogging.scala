e905b7498da6bdf45bc788829ec8bf2a8bb7bebc
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-24 15:31:24 +0530

    Akka logging

---
+++ src/main/scala/akka/actors/ActorLogging.scala
@@ -0,0 +1,40 @@
+package akka.actors
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.event.Logging
+
+object ActorLogging extends App {
+  /**
+   * 1. Explicit logging
+   * */
+  class SimpleActorWithExplicitLogger extends Actor {
+    // 1 - debug
+    // 2 - info
+    // 3 - warn
+    // 4 - error
+    val logger = Logging(context.system, this)
+
+    override def receive: Receive = {
+      case message =>
+        logger.info(message.toString)
+    }
+  }
+
+  /**
+   * 2. Actor logging
+   * */
+  class SimpleActorWithLogging extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case (a, b) => log.info("Two things: {} and {}", a, b)
+      case message =>
+        log.info(message.toString)
+    }
+  }
+
+  val system = ActorSystem("system")
+  val simpleActorWithExplicitLogger = system.actorOf(Props[SimpleActorWithExplicitLogger])
+  val simpleActorWithLogging = system.actorOf(Props[SimpleActorWithLogging])
+  simpleActorWithExplicitLogger ! "[Explicit logging] Hey, log this message"
+  simpleActorWithLogging ! "[Actor logging] Hey, log this message"
+  simpleActorWithLogging ! (1, 2)
+}
