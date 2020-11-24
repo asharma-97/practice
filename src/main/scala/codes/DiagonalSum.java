1b5ba4bc9c5b6cb7099de7752c423cf2de50636d
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-24 06:21:02 +0530

    1572 Diagonal Sum

---
+++ src/main/scala/codes/DiagonalSum.java
@@ -0,0 +1,18 @@
+package codes;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+
+    }
+
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i];
+            if (i != n - i - 1)
+                sum += mat[i][n - i - 1];
+        }
+        return sum;
+    }
+}
