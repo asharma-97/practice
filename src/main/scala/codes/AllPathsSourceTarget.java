faf8a2431da6f52cc94ed0ba6f6e6a7e47cc60f3
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-24 07:05:05 +0530

    797 All path from source to target

---
+++ src/main/scala/codes/AllPathsSourceTarget.java
@@ -0,0 +1,25 @@
+package codes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPathsSourceTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> ll = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
+        helper(graph, 0, l, ll);
+        return ll;
+    }
+
+    public void helper(int[][] graph, int current, List<Integer> l, List<List<Integer>> ll) {
+        l.add(current);
+        if (current == graph.length - 1) {
+            ll.add(l);
+            return;
+        }
+        for (int i = 0; i < graph[current].length; i++) {
+            List<Integer> temp = new ArrayList <>(l);
+            helper(graph, graph[current][i], temp, ll);
+        }
+    }
+}
