b5b3cf8e30272d2c62c954af0f1d685ae362af86
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-25 08:52:40 +0530

    1436 Destination city

---
+++ src/main/scala/codes/DestCity.scala
@@ -0,0 +1,8 @@
+package codes
+
+object DestCity extends App {
+  println(destCity(List(List("London","New York"))))
+  def destCity(paths: List[List[String]]): String = {
+    paths.flatMap(x => if(paths.forall(y => y.head != x(1))) Some(x(1)) else None).head
+  }
+}
