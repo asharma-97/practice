f0591f5edde329da6aa0ff3e4eacac75cf2ba689
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-28 17:49:52 +0530

    1640 Check Array Formation Through Concatenation

---
+++ src/main/scala/codes/CanFormArray.java
@@ -0,0 +1,32 @@
+package codes;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class CanFormArray {
+    public static void main(String[] args) {
+        int[] arr = {91,4,64,78};
+        int[][] pieces = {{78}, {91}, {4,64}};
+        System.out.println(canFormArray(arr, pieces));
+    }
+    public static boolean canFormArray(int[] arr, int[][] pieces) {
+        HashMap<Integer, int[]> hm = new HashMap<>();
+        int[] emptyArray = {};
+        for (int[] piece : pieces) {
+            hm.put(piece[0], piece);
+        }
+        int[] ret = new int[arr.length];
+        for(int i = 0; i< arr.length; i++) {
+            int[] fromMap = hm.getOrDefault(arr[i], emptyArray);
+            for (int k : fromMap) {
+                if(i > arr.length - 1) return false;
+                ret[i] = k;
+                i++;
+            }
+            if(fromMap.length != 0)
+                i--;
+        }
+
+        return Arrays.equals(ret, arr);
+    }
+}
