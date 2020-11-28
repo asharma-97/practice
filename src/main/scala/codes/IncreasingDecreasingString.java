de72860be89e07122327676edea972857df2b256
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-28 07:08:35 +0530

    1370 sort string

---
+++ src/main/scala/codes/IncreasingDecreasingString.java
@@ -0,0 +1,25 @@
+package codes;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class IncreasingDecreasingString {
+    // Use array instead of treemap
+    public String sortString(String s) {
+        StringBuilder ans = new StringBuilder();
+        TreeMap<Character, Integer> tm = new TreeMap<>();
+        for (char c : s.toCharArray()) {
+            tm.put(c, 1 + tm.getOrDefault(c, 0));
+        }
+        boolean alt = true;
+        while (!tm.isEmpty()) {
+            for (char c : alt ? new TreeSet<>(tm.keySet()) : new TreeSet<>(tm.descendingKeySet())) {
+                ans.append(c);
+                tm.put(c, tm.get(c) - 1);
+                tm.remove(c, 0);
+            }
+            alt = !alt;
+        }
+        return ans.toString();
+    }
+}
