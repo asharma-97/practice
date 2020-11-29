34eb6e8744c6888ff6063be4d89e07e3f914051b
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-29 07:47:19 +0530

    1261 Find Elements in a Contaminated Binary Tree

---
+++ src/main/scala/codes/FindElements.scala
@@ -0,0 +1,17 @@
+package codes
+
+object FindElements extends App {
+  val _root: TreeNode = new TreeNode(-1)
+  _root.right = new TreeNode(-1)
+
+  def find(target: Int): Boolean = {
+    helper(_root, target, 0)
+  }
+  def helper(node: TreeNode, target: Int, current: Int): Boolean = {
+    if(node == null) return false
+    if(target == current) return true
+    helper(node.left, target, current * 2 + 1) || helper(node.right, target, current * 2 + 2)
+  }
+
+  println(find(1))
+}
