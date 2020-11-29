f8f2cb1b13202d9eb851cc71e1ec9741adf5a5d4
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-29 08:48:15 +0530

    1325 Delete Leaves With a Given Value

---
+++ src/main/scala/codes/RemoveLeafNodes.scala
@@ -0,0 +1,22 @@
+package codes
+
+object RemoveLeafNodes extends App {
+  def removeLeafNodes(root: TreeNode, target: Int): TreeNode = {
+    if(root == null) return null
+    root.left = removeLeafNodes(root.left,target)
+    root.right = removeLeafNodes(root.right, target)
+    if(root.left == null && root.right == null) {
+      if(root.value == target) {
+        return null
+      }
+    }
+    root
+  }
+  val tn = new TreeNode(1)
+  tn.left = new TreeNode(2)
+  tn.left.left = new TreeNode(2)
+  tn.right = new TreeNode(3)
+  tn.right.left = new TreeNode(2)
+  tn.right.right = new TreeNode(4)
+  removeLeafNodes(tn, 2)
+}
