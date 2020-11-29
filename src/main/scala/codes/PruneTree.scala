f69aee733a6b6b3cf1e03293504638159bf33ba1
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-11-29 11:18:22 +0530

    814 Binary Tree Pruning

---
+++ src/main/scala/codes/PruneTree.scala
@@ -0,0 +1,20 @@
+package codes
+
+object PruneTree extends App {
+  def pruneTree(root: TreeNode): TreeNode = {
+    if(root == null) return null
+    root.left = pruneTree(root.left)
+    root.right = pruneTree(root.right)
+    if(root.left == null && root.right == null && root.value == 0) return null
+    root
+  }
+  val tn = new TreeNode(1)
+  tn.left = new TreeNode(0)
+  tn.left.left = new TreeNode(0)
+  tn.left.right = new TreeNode(0)
+
+  tn.right = new TreeNode(1)
+  tn.right.left = new TreeNode(0)
+  tn.right.right = new TreeNode(1)
+  pruneTree(tn)
+}
