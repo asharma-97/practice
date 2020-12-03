36526201af7e42861216a3c77a15f3c07b5152df
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-12-03 07:02:35 +0530

    104 Maximum Depth of Binary Tree

--- src/main/scala/codes/scala/MaxDepth.scala
+++ src/main/scala/codes/scala/MaxDepth.scala
@@ -1,25 +1,19 @@
 package codes.scala
 
 object MaxDepth extends App {
-
-  class Node(var _value: Int) {
-    var value: Int = _value
-    var children: List[Node] = List()
+  def maxDepth(root: TreeNode): Int = {
+    def helper(node: TreeNode, d: Int): Int = {
+      if(node == null) return d
+      val l = helper(node.left, d + 1)
+      val r = helper(node.right, d + 1)
+      if (l > r) l else r
+    }
+    helper(root, 0)
   }
-
-  def maxDepth(root: Node): Int = {
-    if (root == null) 0
-    else helper(root, 1)
-  }
-
-  def helper(node: Node, depth: Int): Int = {
-    if (node == null) return depth
-    if (node.children.isEmpty) depth
-    else node.children.map(n => helper(n, depth + 1)).max
-  }
-
-  val n = new Node(1)
-  n.children = List(new Node(3), new Node(2), new Node(4))
-  n.children.head.children = List(new Node(5), new Node(6))
-  println(maxDepth(n))
+  val tn = new TreeNode(3)
+  tn.left = new TreeNode(9)
+  tn.right = new TreeNode(20)
+  tn.right.left = new TreeNode(15)
+  tn.right.right = new TreeNode(7)
+  println(maxDepth(tn))
 }
