675684be970abbf991accc73c666695511275e37
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-12-04 22:07:22 +0530

    811 Subdomain Visit Count

--- src/main/scala/codes/scala/UniqueOccurrences.scala
+++ src/main/scala/codes/scala/UniqueOccurrences.scala
@@ -1,7 +1,10 @@
 package codes.scala
 
+import scala.annotation.tailrec
+
 object UniqueOccurrences extends App {
   def uniqueOccurrences(arr: Array[Int]): Boolean = {
+    @tailrec
     def helper(l: List[Int], hm: Map[Int, Int]): Boolean = {
       l match {
         case head :: tail => helper(tail, hm + (head -> (hm.getOrElse(head, 0) + 1)))
