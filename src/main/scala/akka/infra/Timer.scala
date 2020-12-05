84762d3741c0ab28f0aa93f3b26a3610880630ce
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-12-05 09:14:27 +0530

    Timer exercise

---
+++ src/main/scala/akka/infra/Timer.scala
@@ -0,0 +1,39 @@
+package akka.infra
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, PoisonPill, Props}
+
+import scala.concurrent.duration._
+
+object Timer extends App {
+  /**
+   * Exercise: Implement a self closing actor
+   *
+   * - If actor receive a message (anything), you have 1 second to send it another message
+   * - If the time window expires, the actor will stop itself
+   * - If you send another message, the time window resets
+   * */
+  val system = ActorSystem("system")
+  import system.dispatcher
+  class SelfClosingActor extends Actor with ActorLogging{
+    def createTimeoutWindow(): Cancellable = system.scheduler.scheduleOnce(1 second) {
+      self ! PoisonPill
+    }
+    override def receive: Receive = closableActor(createTimeoutWindow())
+    def closableActor(c: Cancellable): Receive = {
+      case message =>
+        log.info(message.toString)
+        c.cancel()
+        context.become(closableActor(system.scheduler.scheduleOnce(1 second) {
+          self ! PoisonPill
+        }))
+    }
+  }
+  val actor = system.actorOf(Props[SelfClosingActor])
+  val routine = system.scheduler.scheduleOnce(0.5 second) {
+    actor ! "PING!"
+  }
+
+  system.scheduler.scheduleOnce(4 second) {
+    actor ! "Are you there?"
+  }
+}
