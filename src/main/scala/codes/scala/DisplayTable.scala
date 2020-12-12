d4797e208b49fb7a995568b58188d0aef93bb732
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-12-12 15:07:52 +0530

    1418 Display Table of Food Orders in a Restaurant

---
+++ src/main/scala/codes/scala/DisplayTable.scala
@@ -0,0 +1,29 @@
+package codes.scala
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object DisplayTable extends App {
+  def displayTable(orders: List[List[String]]): List[List[String]] = {
+    val ts = new mutable.TreeSet[String]()
+    val tm = new mutable.TreeMap[Int, mutable.TreeMap[String, Int]]()
+    orders.foreach { x =>
+      if(tm.contains(x(1).toInt)) {
+        tm(x(1).toInt).update(x(2), tm(x(1).toInt).getOrElse(x(2), 0) + 1)
+      } else {
+        tm.addOne((x(1).toInt, new mutable.TreeMap[String, Int]().addOne(x(2), 1)))
+      }
+    }
+    tm.foreach(x => x._2.keys.foreach(ts.addOne))
+    val l: List[List[String]] = tm.map { x =>
+      val lb = new ListBuffer[String]
+      lb.addOne(x._1.toString)
+      ts.foreach(y => lb.addOne(x._2.getOrElse(y, 0).toString))
+      lb.toList
+    }.toList
+    val x = ("Table" :: ts.toList) :: l
+    x
+  }
+  displayTable(List(List("David","3","Ceviche"), List("Corina","10","Beef Burrito"), List("David","3","Fried Chicken"),
+    List("Carla","5","Water"), List("Carla","5","Ceviche"), List("Rous","3","Ceviche")))
+}
