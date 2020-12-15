02c211d117bec698ff3b0b41700a9015fb976c68
Abhishek Sharma <abhishek.af.sharma@oracle.com>
2020-12-15 10:31:12 +0530

    950 Reveal Cards In Increasing Order

---
+++ src/main/scala/codes/scala/DeckRevealedIncreasing.scala
@@ -0,0 +1,21 @@
+package codes.scala
+
+import scala.collection.mutable
+
+object DeckRevealedIncreasing extends App {
+  def deckRevealedIncreasing(deck: Array[Int]): Array[Int] = {
+    val sorted = deck.sorted
+    val q: mutable.Queue[Int] = new mutable.Queue[Int]()
+    for (i <- sorted.indices) {
+      q.addOne(i)
+    }
+    val ans: Array[Int] = new Array[Int](sorted.length)
+    for (i <- sorted.indices) {
+      ans(q.dequeue()) = sorted(i)
+      if(q.nonEmpty)
+        q.enqueue(q.dequeue())
+    }
+    ans
+  }
+  deckRevealedIncreasing(List(17,13,11,2,3,5,7).toArray)
+}
